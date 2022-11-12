feb5216c22ff4340568568ab1629af8886aa7968
Raphael Tango05 <raphael.tango05@gmail.com>
2022-11-12 12:13:09 +0100

    finish getNumbers and getOperator functions

--- script.js
+++ script.js
@@ -3,50 +3,46 @@ const outputContainer = document.querySelector(".output-container");
 const buttons = document.querySelectorAll("button");
 
 //Variables for whole calculator process (globally for the sake of ease)
-let stringNum= "";
+let stringNum = "";
 let num = [];
-let numCount=0;
-let operator=[];
-let operatorCount=0;
-let opIndex = [];
+let numCount = 0;
+let operator = [];
+let operatorCount = 0;
 
 
 
 //addEventListener for all buttons with further evaluation
 for (let count = 0; count < buttons.length; count++) {
     buttons[count].addEventListener("click", obj => {
-        if( buttons[count].classList.contains("number") ) {
+        if (buttons[count].classList.contains("number")) {
             addToString(buttons[count].value);
-        }
-        else if (buttons[count].classList.contains("operator") ){
-            getNumber();
+        } else if (buttons[count].classList.contains("operator")) {
+            num[numCount]=getNumber(buttons[count].value);
+            console.log(num[numCount]);
+            numCount++;
             addToString(buttons[count].value);
             getOperators(buttons[count].value);
-        }
-        else if (buttons[count].classList.contains("delete") ) {
+        } else if (buttons[count].classList.contains("delete")) {
             deleteAll();
-        }
-        else {
+        } else {
             operateAll();
         }
     });
 }
 
 function operateAll() {
-    
+
 }
 
 //sets obj-values back when "C" button is pressed
 function deleteAll() {
     console.log("delete");
-    obj = {
-        stringNum: "",
-        num: [],
-        operator: [],
-        n: 0,
-        output: ""
-    }
-    console.log(obj);
+    stringNum = "";
+    num = [];
+    numCount = 0;
+    operator = [];
+    operatorCount = 0;
+    opIndex = [];
 
 }
 
@@ -58,26 +54,25 @@ function addToString(item) {
 }
 
 //Calculator Functions
-function add(num1,num2) {
+function add(num1, num2) {
     return num1 + num2;
 }
 
-function sub(num1,num2) {
+function sub(num1, num2) {
     return num1 - num2;
 }
 
-function multiply(num1,num2) {
+function multiply(num1, num2) {
     return num1 * num2;
 }
 
-function divide(num1,num2) {
+function divide(num1, num2) {
     return num1 / num2;
 }
 
 //Get Operator of stringNum after Error-Check
 function getOperators(item) {
-    operator[operatorCount]=item;
-    opIndex[operatorCount]=stringNum.lastIndexOf(item);
+    operator[operatorCount] = item;
     operatorCount++;
 }
 
@@ -88,7 +83,11 @@ function updateOutput() {
 }
 
 //Get last full Number before operator
-function getNumber() {
-    let newString = stringNum.splice();
-    numCount++;
+function getNumber(item) {
+    let newString = stringNum;
+    console.log(newString);
+    if( operatorCount === 0) return Number(newString);
+    else{
+        return Number(newString.slice(stringNum.lastIndexOf(item)-1));
+    }
 }
