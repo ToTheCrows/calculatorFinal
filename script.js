0f7aa0071bbdee4a536bef68f84e3e7ad96f67a6
Raphael Tango05 <raphael.tango05@gmail.com>
2022-11-12 11:10:10 +0100

    restructure js file

--- script.js
+++ script.js
@@ -4,47 +4,45 @@ const buttons = document.querySelectorAll("button");
 
 //Object for whole calculator process (globally for the sake of ease)
 let obj = {
-    stringNum:"",
-    num1:null,
-    num2:null,
-    operator:"",
-    output:""
+    stringNum: "",
+    num: [],
+    operator: [],
+    n: 0,
+    output: "",
+    tempSum:0
 }
 
 //addEventListener for all buttons with further evaluation
-for(let count=0;count<buttons.length;count++) {
+for (let count = 0; count < buttons.length; count++) {
     buttons[count].addEventListener("click", obj => {
-        if( buttons[count].value === "=") operateAll();
-        else if( buttons[count].value === "c" ) deleteAll();
-        else addToString(buttons[count].value);
+        if( buttons[count].classList.contains("number") ) {
+            addToString(buttons[count].value);
+        }
+        else if (buttons[count].classList.contains("operator") ){
+            addToString(buttons[count].value);
+        }
+        else if (buttons[count].classList.contains("delete") ) {
+            deleteAll();
+        }
+        else {
+            operateAll();
+        }
     });
 }
 
 function operateAll() {
-    const outputVar = document. querySelector(".output");
-    let checker = evaluatorError(obj.stringNum);
-
-    console.log(checker);
-    if( checker != 1 ) {
-        deleteAll();
-        outputVar.textContent="ERROR!";
-    }
-    else {
-        obj.operator = getOperator(obj.stringNum);
-    }
-
-
+    
 }
 
 //sets obj-values back when "C" button is pressed
 function deleteAll() {
     console.log("delete");
     obj = {
-        stringNum:"",
-        num1:null,
-        num2:null,
-        operator:"",
-        output:""
+        stringNum: "",
+        num: [],
+        operator: [],
+        n: 0,
+        output: ""
     }
     console.log(obj);
 
@@ -58,47 +56,30 @@ function addToString(item) {
 }
 
 //Calculator Functions
-function add() {
-    return obj.num1+obj.num2;
+function add(num1,num2) {
+    return num1 + num2;
 }
 
-function sub() {
-    return obj.num1-obj.num2;
+function sub(num1,num2) {
+    return num1 - num2;
 }
 
-function multiply() {
-    return obj.num1*obj.num2;
+function multiply(num1,num2) {
+    return num1 * num2;
 }
 
-function divide() {
-    return obj.num1/obj.num2;
-}
-
-//ERROR evaluation if more than one operator
-function evaluatorError(string) {
-    let array = ["+","-","*","/"];
-    let count = 0;
-    array.forEach(element => {
-        for( let n=0;n<string.length;n++){
-            if(element == string[n]) count++;
-        }
-    });
-
-    console.log(count);
-    return count;
+function divide(num1,num2) {
+    return num1 / num2;
 }
 
 //Get Operator of stringNum after Error-Check
-function getOperator(string) {
-    let array = ["+","-","*","/"];
-    let x;
-    x=array.forEach(element => {
-        for(let n=0;n<string.length;n++) {
-            console.log(string[n]);
-            console.log(element);
-            if( string[n]==element ) return n;
-        }
-    });
-    console.log(array[x]);
-    return array[x];
-}   
+function getOperators(item) {
+    operator[n]=item;
+    n++;
+}
+
+//Update output-value on screen
+function updateOutput() {
+    const newOutput = document.querySelector(".output");
+    newOutput.textContent = obj.output;
+}
